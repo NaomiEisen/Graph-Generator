6d63225d95bb327170ea1d92d5b1afe2c552a4fd
Naomi <166138356+NaomiEisen@users.noreply.github.com>
2025-03-18 23:52:04 +0200

    updated comments

--- utils/handle_data.py
+++ utils/handle_data.py
@@ -6,12 +6,6 @@ import os
 def load_data_matrix_format(file_name, start_index=0, end_index=None, offset_right=0):
     """
     Loads data from a .txt or Excel file into a Pandas DataFrame within the specified range of lines.
-
-    :param file_name: The path to the file to load.
-    :param start_index: The starting line index for loading data (inclusive).
-    :param end_index: The ending line index for loading data (inclusive).
-    :param offset_right: Number of values to remove from the beginning of each row (excluding the header).
-    :return: A pandas DataFrame with the data loaded in the specified range.
     """
     try:
         if file_name.endswith((".xls", ".xlsx")):
@@ -56,32 +50,6 @@ def load_data_matrix_format(file_name, start_index=0, end_index=None, offset_rig
     except Exception as e:
         print(f"Error: An unexpected error occurred while loading the file: {e}")
         raise e
-
-
-
-# def load_data_matrix_format(file_name):
-#     """
-#     Loads data from a .txt or Excel file into a Pandas DataFrame.
-#     """
-#     try:
-#         if file_name.endswith((".xls", ".xlsx")):
-#             data = pd.read_excel(file_name, engine="openpyxl")  # Use openpyxl for modern Excel files
-#         else:
-#             data = pd.read_csv(file_name, sep=r'\s+', header=0, skipinitialspace=True)  
-
-#         return data
-#     except FileNotFoundError as e:  # Invalid file name or unavailable file
-#         print(f"Error: The file '{file_name}' was not found.")
-#         raise e
-#     except pd.errors.EmptyDataError as e:  # File available but empty
-#         print("Error: The file is empty.")
-#         raise e
-#     except pd.errors.ParserError as e:  # File format issue
-#         print("Error: The file could not be parsed.")
-#         raise e
-#     except Exception as e:  # Other errors
-#         print("Error: An unexpected error occurred while loading the file.")
-#         raise e
     
     
 def get_files_list(args):
